b43298f8faee0c005c9968cc6d481a70d15db297
luisllamasbinaburo <luisllamas@hotmail.com>
2020-05-04 16:00:00 +0200

    Added TransformOn Render/Layout on WPF

---
+++ XamlFlair.Shared/Entities/TransformationType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamlFlair
+{
+//#if __WPF__
+    public enum TransformationType
+    {
+        Render = 0,
+        Layout,
+    }
+//#endif
+}
